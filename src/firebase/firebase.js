4613f0626268672ec81150a1d5cdd35c1614fc0d
Elizabeth Bonesteel <lizmonster@Aasha.local>
2020-05-15 17:21:32 -0400

    init commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+    // appId: "1:597985732475:web:b4ce35ee9f28aaf3babfde"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export { firebase, googleAuthProvider, database as default };
